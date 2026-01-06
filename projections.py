a3ffc5f8859acce9d7e9ad29dd065ff1b3124846
TheKidjoker <chance.kelly2003@gmail.com>
2026-01-06 15:59:25 -0500

    Renamed vegas_avg to vegas line to represent that this variable will contain the points line that the sports book is giving.

--- projections.py
+++ projections.py
@@ -1,8 +1,8 @@
 ## this function should take in two parameters: The players avearage points per games the vegas line for said player
-def points_prediction(player_pointsavg, vegas_pointsavg):
+def points_prediction(player_pointsavg, vegas_line):
 
-    difference = player_pointsavg - vegas_pointsavg
-    probability = 0.5 + (difference / vegas_pointsavg)
+    difference = player_pointsavg - vegas_line
+    probability = 0.5 + (difference / vegas_line)
     probability= max(0, min(probability, 1))
 
     if probability >= 0.55:
@@ -22,3 +22,11 @@ def points_prediction(player_pointsavg, vegas_pointsavg):
         return "PASS", round(probability * 100, 1)
     
 
+def get_player_data():
+    avg=float(input("Enter player average: "))
+    line=float(input("Enter Vegas line: ")) 
+    return avg, line
+
+
+def cover_rate(game_points, vegas_line):
+    covered= 0
